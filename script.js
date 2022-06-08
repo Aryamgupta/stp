6eddf3aecd765d08a02cae00c55621601ab80e17
pankaj <pankajup.git01@gmail.com>
2022-06-08 08:28:02 +0530

    pankaj added our team section

---
+++ script.js
@@ -0,0 +1,9 @@
+$('#check').change(function(){
+    if ($('#check') .is(":checked")){
+        $('body').css('overflow','hidden');    
+    } 
+    else{ 
+        $('body').css('overflow','auto');    
+    }
+
+});
